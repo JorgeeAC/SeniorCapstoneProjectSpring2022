f774efd9f48201a596e76bfed1904813070b5d7a
Lui332 <84116336+Lui332@users.noreply.github.com>
2022-04-27 18:23:54 -0500

    Create an account for a customer or mechanic

---
+++ CreateFormSuccess.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateFormSuccess = () => {
+  return (
+    <div className="from-content-right">
+      <div className="form-success"> We have recieved your request!</div>
+    </div>
+  );
+};
+
+export default CreateFormSuccess;
