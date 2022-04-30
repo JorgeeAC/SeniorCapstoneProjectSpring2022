bbd78a17b331e468386d2c18a77a7c3e43382c5b
JorgeeAC <jjjcarlos12321@gmail.com>
2022-04-29 21:08:43 -0500

    updated sign in page color

--- frontend/src/components/Signin/SigninElements.js
+++ frontend/src/components/Signin/SigninElements.js
@@ -9,11 +9,7 @@ export const Container = styled.div`
     right: 0;
     z-index: 0;
     overflow: hidden;
-    background: linear-gradient(
-        108deg,
-        rgba(1,147,86,1) 0%,
-        rgba(10,201,122,1) 100%
-    );
+    background-color: #101522;
 `
 
 
